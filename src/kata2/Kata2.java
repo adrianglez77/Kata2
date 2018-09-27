3a7c581e7f0e10bedb3c10f1f7139a3ba0b14705
Usuario <Usuario@10.22.148.103>
2018-09-27 09:29:27 +0100

    Segunda versión de Kata2

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -8,13 +8,11 @@ public class Kata2 {
     public static void main(String[] args) {
         int [] data = {1,2,3,4,5,6,6,5,4,7,8,8,10,24,15};
         Map <Integer, Integer> map = new HashMap<Integer,Integer>();
-        for (int i = 0; i < data.length; i++) {
-            if(map.containsKey(data[i])){
-                map.put(data[i], map.get(data[i])+1);
-            }else{
-                map.put(data[i], 1);
-            }
+        for (int key : data) {
+            map.put(key, map.containsKey(key) 
+                    ? map.get(key) + 1 :1);
         }
+          
         for(Map.Entry<Integer, Integer> entry: map.entrySet()) {
             System.out.println("Key = " + entry.getKey() + ", Value= " + 
                 entry.getValue());} 
